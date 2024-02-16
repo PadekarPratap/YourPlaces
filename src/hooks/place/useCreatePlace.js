c66394adfdf2724dd43865db20921cae8050d60f
Pratap Padekar <unitedpratap7@gmail.com>
2024-02-16 14:21:24 +0530

    completed the project

--- src/hooks/place/useCreatePlace.js
+++ src/hooks/place/useCreatePlace.js
@@ -1,17 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createPlace } from "../../services/apiPlaces";
 import { toast } from "react-toastify";
-import { useParams } from "react-router-dom";
 
 const useCreatePlace = () => {
   const queryClient = useQueryClient();
-  const { userId } = useParams();
 
   const { mutate, isError, isPending, isSuccess, error } = useMutation({
     mutationFn: createPlace,
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["placeByUserId", userId],
+        queryKey: ["placeByUserId"],
       });
       toast.success("Place created successfully");
     },
